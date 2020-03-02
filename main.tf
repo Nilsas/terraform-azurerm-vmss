d72f3f524adb51841ffa65332c02de1134cc2f20
Nilsas <n.firantas@gmail.com>
2020-03-02 21:53:34 +0200

    variablize options in prep to become a module

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  version = "2.0"
+  version = ">=2.0"
 
   features {
     virtual_machine_scale_set {
@@ -8,93 +8,134 @@ provider "azurerm" {
   }
 }
 
-resource "azurerm_resource_group" "rg" {
-  location = "westeurope"
-  name = "${var.prefix}-rg"
-  tags = var.tags
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  address_space = [
-    "12.10.0.0/16"]
-  location = azurerm_resource_group.rg.location
-  name = "${var.prefix}-vnet"
-  resource_group_name = azurerm_resource_group.rg.name
-  tags = var.tags
+  address_space       = [var.address_space]
+  location            = data.azurerm_resource_group.rg.location
+  name                = "${var.prefix}-vnet"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  tags                = var.tags
 }
 
 resource "azurerm_subnet" "subnet" {
-  address_prefix = "12.10.10.0/28"
-  name = "${var.prefix}-subnet"
-  resource_group_name = azurerm_resource_group.rg.name
+  address_prefix       = var.address_space
+  name                 = "${var.prefix}-subnet"
+  resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
 }
 
 resource "azurerm_public_ip" "pip" {
-  location = azurerm_resource_group.rg.location
-  name = "${var.prefix}-pip"
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method = "Dynamic"
+  count               = var.load_balance ? 1 : 0
+  location            = data.azurerm_resource_group.rg.location
+  name                = "${var.prefix}-pip"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
 }
 
 resource "azurerm_lb" "lb" {
-  location = azurerm_resource_group.rg.location
-  name = "${var.prefix}-lb"
-  resource_group_name = azurerm_resource_group.rg.name
+  count               = var.load_balance ? 1 : 0
+  location            = data.azurerm_resource_group.rg.location
+  name                = "${var.prefix}-lb"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  tags                = var.tags
 
   frontend_ip_configuration {
-    name = "${var.prefix}-ip-config"
-    public_ip_address_id = azurerm_public_ip.pip.id
+    name                 = "${var.prefix}-ip-config"
+    public_ip_address_id = element(azurerm_public_ip.pip.*.id, count.index)
   }
 }
 
 resource "azurerm_lb_backend_address_pool" "pool" {
-  loadbalancer_id = azurerm_lb.lb.id
-  name = "${var.prefix}-backend-pool"
-  resource_group_name = azurerm_resource_group.rg.name
+  count               = var.load_balance ? 1 : 0
+  loadbalancer_id     = element(azurerm_lb.lb.*.id, count.index)
+  name                = "${var.prefix}-backend-pool"
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
 
 resource "azurerm_lb_probe" "probe" {
-  name = "${var.prefix}-lb-probe"
-  resource_group_name = azurerm_resource_group.rg.name
-  loadbalancer_id = azurerm_lb.lb.id
-  port = 3200
+  count               = var.load_balance ? 1 : 0
+  name                = "${var.prefix}-lb-probe"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  loadbalancer_id     = element(azurerm_lb.lb.*.id, count.index)
+  port                = var.probe_port
 }
 
 resource "azurerm_linux_virtual_machine_scale_set" "lin_vmss" {
-  name = "${var.prefix}-vmss"
-  resource_group_name = azurerm_resource_group.rg.name
-  location = azurerm_resource_group.rg.location
-  sku = "Standard_B2s"
-  instances = 2
-  admin_username = "joker"
-  tags = var.tags
+  name                = "${var.prefix}-vmss"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  sku                 = var.vm_size
+  instances           = var.instance_count
+  admin_username      = "joker"
+  tags                = var.tags
 
   admin_ssh_key {
-    username = "joker"
+    username   = "joker"
     public_key = file("./id_rsa.pub")
   }
 
   source_image_reference {
-    publisher = "Canonical"
-    offer = "UbuntuServer"
-    sku = "18.04-LTS"
-    version = "latest"
+    publisher = var.linux_distro_list[lower(var.linux_distro)]["publisher"]
+    offer     = var.linux_distro_list[lower(var.linux_distro)]["offer"]
+    sku       = var.linux_distro_list[lower(var.linux_distro)]["sku"]
+    version   = var.linux_distro_list[lower(var.linux_distro)]["version"]
   }
 
   os_disk {
     storage_account_type = "Standard_LRS"
-    caching = "ReadWrite"
+    caching              = "ReadWrite"
   }
 
   network_interface {
-    name = "${var.prefix}-nic"
+    name    = "${var.prefix}-nic"
     primary = true
 
     ip_configuration {
-      name = "internal"
-      primary = true
-      subnet_id = azurerm_subnet.subnet.id
+      name                                   = "internal"
+      primary                                = true
+      subnet_id                              = azurerm_subnet.subnet.id
+      load_balancer_backend_address_pool_ids = list(azurerm_lb_backend_address_pool.pool.id)
+    }
+  }
+}
+
+resource "azurerm_windows_virtual_machine_scale_set" "lin_vmss" {
+  name                = "${var.prefix}-vmss"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  sku                 = "Standard_B2s"
+  instances           = 2
+  admin_username      = "joker"
+  tags                = var.tags
+
+  admin_ssh_key {
+    username   = "joker"
+    public_key = file("./id_rsa.pub")
+  }
+
+  source_image_reference {
+    publisher = var.win_distro_list[lower(var.win_distro)]["publisher"]
+    offer     = var.win_distro_list[lower(var.win_distro)]["offer"]
+    sku       = var.win_distro_list[lower(var.win_distro)]["sku"]
+    version   = var.win_distro_list[lower(var.win_distro)]["version"]
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  network_interface {
+    name    = "${var.prefix}-nic"
+    primary = true
+
+    ip_configuration {
+      name                                   = "internal"
+      primary                                = true
+      subnet_id                              = azurerm_subnet.subnet.id
       load_balancer_backend_address_pool_ids = list(azurerm_lb_backend_address_pool.pool.id)
     }
   }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,127 @@
+variable "location" {
+  type        = string
+  default     = "westeurope"
+  description = "This variable will point all resource into one Azure location"
+}
+
+variable "resource_group_name" {
+  type        = string
+  default     = "my-resource-group"
+  description = "This will tell us to which resource group we need to deploy the resources of this module"
+}
+
+variable "address_space" {
+  type        = string
+  default     = "10.10.0.0/16"
+  description = "This is to create a Vnet and Subnet for our Scale Set"
+}
+
 variable "prefix" {
-  type = string
-  default = "my-prefix"
+  type        = string
+  default     = "my-prefix"
   description = "This variable is used to unify all resource naming within this module"
 }
 
 variable "tags" {
-  type = map(string)
-  default = {}
+  type        = map(string)
+  default     = {}
   description = "This variable is used to refference same tags through all resources"
+}
+
+variable "load_balance" {
+  type        = bool
+  default     = false
+  description = "This ether enables or disabels the load balancer building"
+}
+
+variable "probe_port" {
+  type        = string
+  default     = "80"
+  description = "If load_balancer is set to true it will need a port to probe in order for load balancer to work"
+}
+
+variable "vm_size" {
+  type        = string
+  default     = "Standard_B2s"
+  description = "This desides what size your VMs will be"
+}
+
+variable "instance_count" {
+  type        = number
+  default     = 1
+  description = "This decides how many VM instance should be spun up"
+}
+
+variable "flavour" {
+  type        = string
+  default     = "linux"
+  description = "This is needed to decide which flavour of VMSS to deploy Windows or Linux"
+}
+
+
+variable "linux_distro_list" {
+  type = map(object({
+    publisher = string
+    offer     = string
+    sku       = string
+    version   = string
+  }))
+
+  default = {
+    ubuntu1804 = {
+      publisher = "Canonical"
+      offer     = "UbuntuServer"
+      sku       = "18.04-LTS"
+      version   = "latest"
+    }
+
+    centos8 = {
+      publisher = "OpenLogic"
+      offer     = "CentOS"
+      sku       = "8.0"
+      version   = "latest"
+    }
+  }
+}
+
+variable "linux_distro" {
+  type    = string
+  default = "centos8"
+}
+
+variable "win_distro_list" {
+  type = map(object({
+    publisher = string
+    offer     = string
+    sku       = string
+    version   = string
+  }))
+
+  default = {
+    winserver = {
+      publisher = "MicrosoftWindowsServer"
+      offer     = "WindowsServer"
+      sku       = "2019-Datacenter"
+      version   = "latest"
+    }
+
+    wincore = {
+      publisher = "MicrosoftWindowsServer"
+      offer     = "WindowsServer"
+      sku       = "2016-Datacenter-Server-Core"
+      version   = "latest"
+    }
+
+    winsql = {
+      publisher = "MicrosoftSQLServer"
+      offer     = "SQL2017-WS2016"
+      sku       = "Express"
+      version   = "latest"
+    }
+  }
+}
+
+variable "win_distro" {
+  type    = string
+  default = "wincore"
 }
