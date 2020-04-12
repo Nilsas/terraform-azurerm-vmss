1aef86861b1f16517fcc7b918c5c4d1caae93e4c
Nilsas <n.firantas@gmail.com>
2020-04-12 20:32:15 +0300

    added terratest

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "ssh_key_public" {
 output "ssh_key_private" {
   value = lower(var.ssh_key_type) == "generated" ? tls_private_key.ssh[0].private_key_pem : null
 }
+
+output "pip" {
+  value = azurerm_public_ip.pip
+}
---
+++ tests/fixture_lin/main.tf
@@ -0,0 +1,42 @@
+provider "azurerm" {
+  version = ">= 2.0.0"
+
+  features {
+    virtual_machine_scale_set {
+      roll_instances_when_required = false
+    }
+  }
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = "westeurope"
+  name     = "terraform-test-rg"
+  tags = {
+    EnvironmentType = "Development"
+  }
+}
+
+module "vmss" {
+  source                  = "../../"
+  prefix                  = "nil"
+  flavour                 = "lin"
+  instance_count          = 2
+  load_balance            = true
+  load_balanced_port_list = [22, 80]
+  ssh_key_type            = "Generated"
+  resource_group_name     = azurerm_resource_group.rg.name
+  admin_username          = "batman"
+  tags                    = azurerm_resource_group.rg.tags
+}
+
+output "ssh_pub_key" {
+  value = module.vmss.ssh_key_public
+}
+
+output "ssh_priv_key" {
+  value = module.vmss.ssh_key_private
+}
+
+output "public_ip_address" {
+  value = module.vmss.pip.ip_address
+}
