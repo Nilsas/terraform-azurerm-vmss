21af12eb9e682453fde5758fa871e49b8043e0dc
Nilsas <n.firantas@gmail.com>
2020-04-16 17:35:24 +0300

    func to retry ssh hosts

--- tests/terraform_ssh_test.go
+++ tests/terraform_ssh_test.go
@@ -38,19 +38,8 @@ func testSSHToPublicHost(t *testing.T, terraformOptions *terraform.Options, sshI
 
 	keyPair := ssh.KeyPair{PrivateKey: string(buffer)}
 
-	// Read public IP address and port from terraform output
-	hostsResult := terraform.Output(t, terraformOptions, sshInfo)
+	sshHosts := getInstanceConnectionInfo(t, terraformOptions, sshInfo)
 
-	// Clean up hosts result
-	hostsResult = strings.ReplaceAll(hostsResult, "\n", "")
-	hostsResult = strings.ReplaceAll(hostsResult, " ", "")
-	hostsResult = strings.ReplaceAll(hostsResult, `""`, `","`)
-	hostsResult = strings.ReplaceAll(hostsResult, "{", "")
-	hostsResult = strings.ReplaceAll(hostsResult, "}", "")
-	hostsResult = strings.ReplaceAll(hostsResult, `"`, "")
-
-	// Split up cleaned result
-	sshHosts := strings.Split(hostsResult, ",")
 	wg := &sync.WaitGroup{}
 	for _, instance := range sshHosts {
 		wg.Add(1)
@@ -93,4 +82,37 @@ func testSSHToPublicHost(t *testing.T, terraformOptions *terraform.Options, sshI
 		}(instance)
 	}
 	wg.Wait()
+}
+
+func getInstanceConnectionInfo(t *testing.T, terraformOptions *terraform.Options, outputName string) []string {
+	// Read public IP address and port from terraform output
+	hostsResult := terraform.Output(t, terraformOptions, outputName)
+
+	// Clean up hosts result
+	hostsResult = strings.ReplaceAll(hostsResult, "\n", "")
+	hostsResult = strings.ReplaceAll(hostsResult, " ", "")
+	hostsResult = strings.ReplaceAll(hostsResult, `""`, `","`)
+	hostsResult = strings.ReplaceAll(hostsResult, "{", "")
+	hostsResult = strings.ReplaceAll(hostsResult, "}", "")
+	hostsResult = strings.ReplaceAll(hostsResult, `"`, "")
+
+	// Split up cleaned result
+	sshHosts := strings.Split(hostsResult, ",")
+
+	// Test case is carried with 2 instances
+	// If there are more than 2 instance in the list, wait
+	// Azure over-provisions Instances to maximize success rate
+	// retry for 5 times
+	for i := 0; i < 5 ; i++ {
+		if len(sshHosts) > 2 {
+			time.Sleep(10 * time.Second)
+			fmt.Println("Sleeping for 10 seconds")
+		} else if len(sshHosts) == 2 {
+			break
+		} else {
+			fmt.Println("SSH Hosts count did not match expectations")
+		}
+	}
+
+	return sshHosts
 }
