0d50e4b6c9f832fe1cacb5de447284924d46373b
Nilsas <n.firantas@gmail.com>
2020-04-18 22:45:32 +0300

    win test only applies and destroys

--- tests/fixture_win/main.tf
+++ tests/fixture_win/main.tf
@@ -4,6 +4,7 @@ module "vmss_win" {
   resource_group_name     = azurerm_resource_group.rg_win.name
   virtual_network_name    = azurerm_virtual_network.vnet_win.name
   subnet_name             = azurerm_subnet.subnet_win.name
+  overprovision           = false
   flavour                 = "win"
   instance_count          = 2
   admin_username          = var.admin_user
@@ -12,4 +13,32 @@ module "vmss_win" {
   load_balance            = true
   load_balanced_port_list = [80,443]
   enable_nat              = true
+}
+
+resource "null_resource" "delay" {
+  // Might want to wait until Azure is done provisioning the instances
+  provisioner "local-exec" {
+    command = "Start-Sleep 60"
+    interpreter = ["pwsh", "-Command"]
+  }
+  depends_on = [module.vmss_win.vmss]
+}
+
+data "external" "list_win_vmss_ips" {
+  program = [
+    "pwsh",
+    "-Command",
+    "az",
+    "vmss",
+    "list-instance-connection-info",
+    "-g",
+    azurerm_resource_group.rg_win.name,
+    "--name",
+    "${local.prefix}-vmss",
+  ]
+  depends_on = [null_resource.delay]
+}
+
+output "winrm_conn_info" {
+  value = data.external.list_win_vmss_ips.result
 }
--- tests/terraform_winrm_test.go
+++ tests/terraform_winrm_test.go
@@ -1,7 +1,10 @@
 package test
 
 import (
+	"fmt"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
@@ -18,5 +21,41 @@ func TestTerraformWinRM(t *testing.T) {
 
 	terraform.InitAndApply(t, terraformOptions)
 	defer terraform.Destroy(t, terraformOptions)
+	hosts := getInstanceConnInfo(t, terraformOptions)
+	fmt.Println(hosts)
+}
 
+func getInstanceConnInfo(t *testing.T, terraformOptions *terraform.Options) []string  {
+	var winrmHosts []string
+	connInfo := "winrm_conn_info"
+	for i := 0; i < 5 ; i++ {
+		// set terraform options to target just the resource I need
+
+		hostsResult := terraform.Output(t, terraformOptions, connInfo)
+
+		// Clean up hosts result
+		hostsResult = strings.ReplaceAll(hostsResult, "\n", "")
+		hostsResult = strings.ReplaceAll(hostsResult, " ", "")
+		hostsResult = strings.ReplaceAll(hostsResult, `""`, `","`)
+		hostsResult = strings.ReplaceAll(hostsResult, "{", "")
+		hostsResult = strings.ReplaceAll(hostsResult, "}", "")
+		hostsResult = strings.ReplaceAll(hostsResult, `"`, "")
+
+		// Split up cleaned result
+		winrmHosts := strings.Split(hostsResult, ",")
+		fmt.Println("Current winrm hosts:")
+		fmt.Println(winrmHosts)
+
+		if len(winrmHosts) > 2 {
+			fmt.Println("Sleeping for 30 seconds")
+			time.Sleep(30 * time.Second)
+
+		} else if len(winrmHosts) == 2 {
+			break
+		} else {
+			fmt.Println("WinRM Hosts count did not match expectations")
+		}
+	}
+
+	return winrmHosts
 }
